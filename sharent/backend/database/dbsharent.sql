4a54e54a2e5e202afbf206f8c50d2dcfe887d854
Paulo Oliveira <paulinho@Paulos-iMac.local>
2019-06-28 10:25:19 -0400

    Update database

--- sharent/backend/database/dbsharent.sql
+++ sharent/backend/database/dbsharent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 25, 2019 at 05:03 PM
+-- Generation Time: Jun 28, 2019 at 02:22 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -13,6 +13,68 @@ SET time_zone = "+00:00";
 --
 -- Database: `dbsharent`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `recipientId` int(11) NOT NULL,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `isRead` char(1) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productEvaluations`
+--
+
+CREATE TABLE `productEvaluations` (
+  `id` int(11) NOT NULL,
+  `evaluatorUser` int(11) NOT NULL,
+  `evaluatedProduct` int(11) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `evaluation` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `product_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `imagePath` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userEvaluations`
+--
+
+CREATE TABLE `userEvaluations` (
+  `id` int(11) NOT NULL,
+  `evaluatorUser` int(11) NOT NULL,
+  `evaluatedUser` int(11) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `evaluation` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -31,6 +93,41 @@ CREATE TABLE `users` (
 
 
 --
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `messages_ibfk_1` (`userId`),
+  ADD KEY `messages_ibfk_2` (`recipientId`);
+
+--
+-- Indexes for table `productEvaluations`
+--
+ALTER TABLE `productEvaluations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productEvaluations_ibfk_1` (`evaluatorUser`),
+  ADD KEY `productEvaluations_ibfk_2` (`evaluatedProduct`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_ibfk_1` (`userId`);
+
+--
+-- Indexes for table `userEvaluations`
+--
+ALTER TABLE `userEvaluations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userEvaluations_ibfk_1` (`evaluatorUser`),
+  ADD KEY `userEvaluations_ibfk_2` (`evaluatedUser`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -41,7 +138,56 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `userEvaluations`
+--
+ALTER TABLE `userEvaluations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`recipientId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `productEvaluations`
+--
+ALTER TABLE `productEvaluations`
+  ADD CONSTRAINT `productEvaluations_ibfk_1` FOREIGN KEY (`evaluatorUser`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `productEvaluations_ibfk_2` FOREIGN KEY (`evaluatedProduct`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `userEvaluations`
+--
+ALTER TABLE `userEvaluations`
+  ADD CONSTRAINT `userEvaluations_ibfk_1` FOREIGN KEY (`evaluatorUser`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userEvaluations_ibfk_2` FOREIGN KEY (`evaluatedUser`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
