3a309b9b839b1c3e263e87aa28268b6199709aaf
Paulo Oliveira <paulinho@Paulos-iMac.local>
2019-07-11 20:05:46 -0400

    Guard the Menu routes

--- sharent/src/app/adminPages/admin.routing.ts
+++ sharent/src/app/adminPages/admin.routing.ts
@@ -5,6 +5,7 @@ import { ReviewsComponent } from './Reviews/Reviews.component';
 import { ListComponent } from './List/List.component';
 import { AddListComponent } from './AddList/AddList.component';
 import { ProfileComponent } from './Profile/Profile.component';
+import { AuthGuard } from '../session/auth.guard';
 
 export const AdminRoutes: Routes = [
 {
@@ -17,10 +18,15 @@ export const AdminRoutes: Routes = [
 },
 {
   path: 'list',
-  component: ListComponent
+  component: ListComponent,
+  canActivate: [AuthGuard]
 },
 {
   path: 'add-list',
+  component: AddListComponent
+},
+{
+  path: 'edit/:prodId',
   component: AddListComponent
 },
 {
--- sharent/src/app/core/Menu/Menu.component.ts
+++ sharent/src/app/core/Menu/Menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { MenuItems } from './menu-items';
+import { AuthService } from 'src/app/session/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -11,10 +13,18 @@ export class MenuComponent implements OnInit {
 
    selectedMenu: any = null;
    selectedSubMenu: any = null;
-   constructor(public menuItems: MenuItems){}
+   userIsAuthenticated = false;
+   private authListenerSubs: Subscription;
+
+   constructor(public menuItems: MenuItems, public authService: AuthService) {}
 
    ngOnInit(){
-      
+      this.userIsAuthenticated = this.authService.getIsAuth();
+      this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
    }
 
    menuClick(value)
